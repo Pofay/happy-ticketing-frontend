8aa3d67778e15a5d9c083bff8665aba27a77fee4
pofay <gianCarlo.gilos@gmail.com>
2019-08-28 14:17:36 +0800

    Tweaked Login Portal

--- src/pages/Index/index.js
+++ src/pages/Index/index.js
@@ -11,39 +11,69 @@ const useStyles = makeStyles(theme => ({
     maxWidth: 500,
     maxHeight: 500,
     margin: 'auto',
-    marginTop: '5%',
-    padding: '2%'
+    marginTop: '5%'
   },
   paper: {
-    height: '100%',
-    maxHeight: 600
+    height: 120,
+    padding: '2%'
   },
   login: {
-    marginTop: '0.75%'
+    marginTop: '2%'
   }
 }));
+
+const LoggedInPrompt = props => {
+  const classes = useStyles();
+  const { logout } = useAuth0();
+
+  return (
+    <>
+      <Typography variant="h6">You are currently logged in.</Typography>
+
+      <Button
+        className={classes.login}
+        variant="contained"
+        onClick={() => logout()}
+      >
+        Log out
+      </Button>
+    </>
+  );
+};
+
+const LoggedOutPrompt = props => {
+  const { loginWithRedirect } = useAuth0();
+  const classes = useStyles();
+
+  return (
+    <>
+      <Typography variant="h6">You are not logged in.</Typography>
+      <Button
+        onClick={() => loginWithRedirect({})}
+        className={classes.login}
+        variant="contained"
+      >
+        Login
+      </Button>
+    </>
+  );
+};
 const Index = props => {
   const classes = useStyles();
-  const { isAuthenticated, loginWithRedirect, logout, loading } = useAuth0();
+  const { isAuthenticated, loading } = useAuth0();
 
   return loading ? (
     <div />
   ) : (
     <div className={classes.root}>
-      <Paper className={classes.login}>
+      <Paper className={classes.paper}>
         <Typography variant="h5">
-          Welcome to the Happy Ticketing Frontend
+          Welcome to the Happy Ticketing Frontend.
         </Typography>
-        {!isAuthenticated && (
-          <Button
-            onClick={() => loginWithRedirect({})}
-            className={classes.login}
-            variant="contained"
-          >
-            Login
-          </Button>
-        )}
-        {isAuthenticated && <Button variant="contained" onClick={() => logout()}>Log out</Button>}
+
+        {!isAuthenticated && <LoggedOutPrompt />}
+
+        {isAuthenticated && <LoggedInPrompt />}
       </Paper>
     </div>
   );
