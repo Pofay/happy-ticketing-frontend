f260a2ac84293fccdc850d3c4b25683f9423b359
pofay <gianCarlo.gilos@gmail.com>
2019-09-11 12:49:16 +0800

    Added logout in MenuBar

--- src/components/MenuBar/index.js
+++ src/components/MenuBar/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import IconButton from '@material-ui/core/IconButton';
+import { useAuth0 } from '../auth0-wrapper';
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  IconButton,
+  Button
+} from '@material-ui/core';
 import MenuIcon from '@material-ui/icons/Menu';
 
 const useStyles = makeStyles(theme => ({
@@ -21,6 +25,7 @@ const useStyles = makeStyles(theme => ({
 
 const MenuBar = props => {
   const classes = useStyles();
+  const { logout } = useAuth0();
 
   return (
     <AppBar position="static" className={classes.appBar}>
@@ -36,6 +41,9 @@ const MenuBar = props => {
         <Typography variant="h6" className={classes.title}>
           {props.title}
         </Typography>
+        <Button edge="end" variant="contained" color="secondary" onClick={() => logout()}>
+          Log out
+        </Button>
       </Toolbar>
     </AppBar>
   );
